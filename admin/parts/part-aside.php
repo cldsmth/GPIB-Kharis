bab33d1eb6059ef78dfbaec842f242bdb35c7d7c
Cliff Dean Smith <dean11.cliff@gmail.com>
2018-05-22 20:05:10 +0700

    connect session login admin name

--- admin/parts/part-aside.php
+++ admin/parts/part-aside.php
@@ -9,7 +9,7 @@
       </div>
       <div class="media-body">
         <div class="foldable">
-          <h5><a href="javascript:void(0)" class="username">John Doe</a></h5>
+          <h5><a href="javascript:void(0)" class="username"><?=$_SESSION['GpibKharis']['admin']['name'];?></a></h5>
           <ul>
             <li class="dropdown">
               <a href="javascript:void(0)" class="dropdown-toggle usertitle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
