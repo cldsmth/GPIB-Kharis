178b18c10dc8cb9fb2ff0490e9ba9587ec1709b2
Cliff Dean Smith <dean11.cliff@gmail.com>
2018-07-25 15:46:25 +0700

    dashboard ibadah pelkat pkp periode maret - juni 2018

---
+++ dashboard/ibadah-pelkat/2018/march_june/PKP/js/chart.js
@@ -0,0 +1,59 @@
+Highcharts.chart('container', {
+	chart: {
+	    type: 'column'
+	},
+	title: {
+	    text: 'Jumlah Kehadiran Ibadah Pelkat PKP<br>Periode: Maret - Juni 2018'
+	},
+	subtitle: {
+	    text: 'Source: GPIB Kharis Pulo Gebang'
+	},
+	xAxis: {
+	    categories: [
+	        '20-Mar-2018',
+	        '27-Mar-2018',
+	        '3-Apr-2018',
+	        '10-Apr-2018',
+	        '17-Apr-2018',
+	        '24-Apr-2018',
+	        '1-May-2018',
+	        '8-May-2018',
+	        '15-May-2018',
+	        '22-May-2018',
+	        '29-May-2018',
+	        '5-Jun-2018',
+	        '12-Jun-2018',
+	        '19-Jun-2018',
+	        '26-Jun-2018',
+	    ],
+	    crosshair: true
+	},
+	yAxis: {
+	    min: 0,
+	    title: {
+	        text: 'Jumlah Kehadiran'
+	    }
+	},
+	tooltip: {
+	    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+	    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+	        '<td style="padding:0"><b>{point.y} Jemaat</b></td></tr>',
+	    footerFormat: '</table>',
+	    shared: true,
+	    useHTML: true
+	},
+	plotOptions: {
+	    column: {
+	        pointPadding: 0.2,
+	        borderWidth: 0,
+	        dataLabels: {
+                enabled: true
+            }
+	    }
+	},
+	series: [{
+	    name: 'PKP',
+	    color: '#9C27B0',
+	    data: [19, 23, 23, 33, 25, 32, 16, 27, 21, 21, 16, 14, 12, 19, 17]
+	}]
+});
