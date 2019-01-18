a3c0efdd7b5d6010288e2170885d925408cba124
Cliff Dean <cliff.dean@eannovate.com>
2019-01-18 10:35:57 +0700

    init front-end files

---
+++ parts/part-head.php
@@ -0,0 +1,14 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="assets/image/favicon.png" type="image/png">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="assets/css/bootstrap.css">
+<link rel="stylesheet" href="assets/vendors/linericon/style.css">
+<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/vendors/lightbox/simpleLightbox.css">
+<link rel="stylesheet" href="assets/vendors/nice-select/css/nice-select.css">
+<!-- main css -->
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
---
+++ parts/part-header.php
@@ -0,0 +1,69 @@
+<header class="header_area">
+    <div class="header_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-5">
+                    <ul class="nav social_icon">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-7">
+                    <div class="top_btn d-flex justify-content-end">
+                        <a href="#">My Account</a>
+                        <a href="#">Donate Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <a class="navbar-brand logo_h" href="index.html"><img src="assets/image/Logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                <ul class="nav navbar-nav menu_nav ml-auto">
+                    <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
+                    <li class="nav-item"><a class="nav-link" href="ministries.html">Minisrtries</a></li>
+                    <li class="nav-item"><a class="nav-link" href="sermons.html">Sermons</a></li>
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Event</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="event.html">Event</a></li>
+                            <li class="nav-item"><a class="nav-link" href="event-details.html">Event Details</a></li>
+                        </ul>
+                    </li> 
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="about-us.html">About</a></li>
+                            <li class="nav-item"><a class="nav-link" href="donation.html">Donation</a></li>
+                            <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                            <li class="nav-item"><a class="nav-link" href="elements.html">Elements</a></li>
+                        </ul>
+                    </li> 
+                    <li class="nav-item submenu dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                        <ul class="dropdown-menu">
+                            <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                            <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
+                        </ul>
+                    </li> 
+                    
+                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item"><a href="#" class="search"><i class="lnr lnr-magnifier"></i></a></li>
+                </ul>
+            </div> 
+        </div>
+    </nav>
+</header>
